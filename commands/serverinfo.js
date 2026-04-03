137fc76981b79f639c56c04f63de65bcc53663d5
Axados1111 <axadosstudios@outlook.com>
2026-04-03 14:58:26 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,1 +1,13 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+    data: new SlashCommandBuilder()
+        .setName('serverinfo')
+        .setDescription('Get server info'),
+    async execute(interaction) {
+        const guild = interaction.guild;
+        await interaction.reply(
+            `**Server Info:**\nName: ${guild.name}\nID: ${guild.id}\nMembers: ${guild.memberCount}\nCreated: ${guild.createdAt.toDateString()}`
+        );
+    }
+};
