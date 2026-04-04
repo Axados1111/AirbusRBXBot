e463e7fc76889b82d805f60d860c1b3ce26dbd9e
Axados1111 <axadosstudios@outlook.com>
2026-04-04 17:33:04 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,26 +1,31 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('kick')
-        .setDescription('Kick a user from the server')
-        .addUserOption(opt => opt.setName('target').setDescription('User to kick').setRequired(true))
-        .addStringOption(opt => opt.setName('reason').setDescription('Reason for kick')),
-    async execute(interaction) {
-        if (!interaction.member.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return interaction.reply({ content: 'You do not have permission to kick members.', ephemeral: true });
-        }
-        const user = interaction.options.getUser('target');
-        const member = interaction.guild.members.cache.get(user.id);
-        const reason = interaction.options.getString('reason') || 'No reason provided';
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Ban a user')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to ban')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 
-        if (!member) return interaction.reply('User not found.');
-        try {
-            await member.kick(reason);
-            await interaction.reply(`Kicked ${user.tag} | Reason: ${reason}`);
-        } catch (err) {
-            console.error(err);
-            await interaction.reply('Failed to kick the user.');
-        }
+  async execute(interaction) {
+    const member = interaction.options.getMember('user');
+
+    if (!member) {
+      return interaction.reply({ content: 'User not found in this server.', ephemeral: true });
     }
+
+    if (!member.bannable) {
+      return interaction.reply({
+        content: 'I cannot ban this user. Check role hierarchy or permissions.',
+        ephemeral: true
+      });
+    }
+
+    await member.ban();
+    await interaction.reply(`🔨 Banned ${member.user.tag}`);
+  }
 };
