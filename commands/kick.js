e12301dbe1d1efc602a38af71de946d3b6ecd4a4
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:38:02 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,31 +1,14 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-export default {
+module.exports = {
   data: new SlashCommandBuilder()
     .setName('kick')
     .setDescription('Kick a user')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('User to kick')
-        .setRequired(true)
-    )
+    .addUserOption(o => o.setName('user').setDescription('User').setRequired(true))
     .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
   async execute(interaction) {
-    const member = interaction.options.getMember('user');
-
-    if (!member) {
-      return interaction.reply({ content: 'User not found in this server.', ephemeral: true });
-    }
-
-    if (!member.kickable) {
-      return interaction.reply({
-        content: 'I cannot kick this user. Check role hierarchy or permissions.',
-        ephemeral: true
-      });
-    }
-
-    await member.kick();
-    await interaction.reply(`✅ Successfully Kicked ${member.user.tag}`);
+    const user = interaction.options.getUser('user');
+    await interaction.reply(`${user.tag} would be kicked (placeholder)`);
   }
 };
