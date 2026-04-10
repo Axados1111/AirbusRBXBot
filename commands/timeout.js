58de09843fa55fdf63f06aa40ed50df20d234ede
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:39:09 +0100

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,27 +1,14 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-export default {
-    data: new SlashCommandBuilder()
-        .setName('timeout')
-        .setDescription('Timeout a user')
-        .addUserOption(opt => opt.setName('target').setDescription('User to timeout').setRequired(true))
-        .addIntegerOption(opt => opt.setName('duration').setDescription('Duration in minutes').setRequired(true)),
-    async execute(interaction) {
-        if (!interaction.member.permissions.has(PermissionFlagsBits.ModerateMembers)) {
-            return interaction.reply({ content: 'You do not have permission to timeout members.', ephemeral: true });
-        }
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('timeout')
+    .setDescription('Timeout a user')
+    .addUserOption(o => o.setName('user').setDescription('User').setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 
-        const user = interaction.options.getUser('target');
-        const member = interaction.guild.members.cache.get(user.id);
-        const duration = interaction.options.getInteger('duration');
-
-        if (!member) return interaction.reply('User not found.');
-        try {
-            await member.timeout(duration * 60 * 1000, `Timed out by ${interaction.user.tag}`);
-            await interaction.reply(`${user.tag} has been timed out for ${duration} minutes.`);
-        } catch (err) {
-            console.error(err);
-            await interaction.reply('Failed to timeout the user.');
-        }
-    }
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    await interaction.reply(`${user.tag} would be timed out (placeholder)`);
+  }
 };
