5551fd96b9a4d7f7e58c089fb2abd322b8afbddb
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:40:51 +0100

    Update warnings.js

--- commands/warnings.js
+++ commands/warnings.js
@@ -1,20 +1,11 @@
-import { SlashCommandBuilder } from 'discord.js';
-import fs from 'fs';
+const { SlashCommandBuilder } = require('discord.js');
 
-export default {
-    data: new SlashCommandBuilder()
-        .setName('warnings')
-        .setDescription('Check warnings for a user')
-        .addUserOption(opt => opt.setName('target').setDescription('User to check').setRequired(true)),
-    async execute(interaction) {
-        const user = interaction.options.getUser('target');
-        const warnings = JSON.parse(fs.readFileSync('warnings.json'));
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('warnings')
+    .setDescription('Check warnings'),
 
-        if (!warnings[user.id] || warnings[user.id].length === 0) {
-            return interaction.reply(`${user.tag} has no warnings.`);
-        }
-
-        const warnList = warnings[user.id].map((w, i) => `${i + 1}. ${w.reason} (by ${w.moderator} on ${w.date.split('T')[0]})`).join('\n');
-        await interaction.reply(`Warnings for ${user.tag}:\n${warnList}`);
-    }
+  async execute(interaction) {
+    await interaction.reply('No warnings found (placeholder)');
+  }
 };
