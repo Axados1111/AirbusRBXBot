b88afa3539c9d03bd7a4a2f3cd580249061959bb
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:37:39 +0100

    Update clearwarnings.js

--- commands/clearwarnings.js
+++ commands/clearwarnings.js
@@ -1,42 +1,11 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const fs = require('fs');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('clearwarnings')
-    .setDescription('Clear all warnings for a user')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('User to clear warnings for')
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    .setDescription('Clear warnings'),
 
   async execute(interaction) {
-    const target = interaction.options.getUser('user');
-
-    // Load warnings
-    let warnings = {};
-    if (fs.existsSync('./warnings.json')) {
-      warnings = JSON.parse(fs.readFileSync('./warnings.json'));
-    }
-
-    // Clear warnings
-    warnings[target.id] = 0;
-
-    // Save file
-    fs.writeFileSync('./warnings.json', JSON.stringify(warnings, null, 2));
-
-    // Log channel (replace with your channel ID)
-    const logChannel = interaction.guild.channels.cache.get('LOG_CHANNEL_ID');
-
-    if (logChannel) {
-      logChannel.send(`${target.tag}'s warnings were cleared by ${interaction.user.tag}`);
-    }
-
-    await interaction.reply({
-      content: `✅ Cleared all warnings for ${target.tag}`,
-      ephemeral: true
-    });
+    await interaction.reply('Warnings cleared (placeholder)');
   }
 };
