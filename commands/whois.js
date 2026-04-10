52ea52516ce2f08fa11d0849192e9063e660baa4
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:41:08 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,36 +1,13 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('whois')
-    .setDescription('Get detailed info about a user')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('User to lookup')
-        .setRequired(true)
-    ),
+    .setDescription('User info lookup')
+    .addUserOption(o => o.setName('user').setDescription('User')),
 
   async execute(interaction) {
-    const user = interaction.options.getUser('user');
-    const member = interaction.guild.members.cache.get(user.id);
-
-    const embed = new EmbedBuilder()
-      .setColor(0x2b2d31)
-      .setTitle(`🔍 User Info: ${user.tag}`)
-      .setThumbnail(user.displayAvatarURL())
-      .addFields(
-        { name: 'User ID', value: user.id, inline: true },
-        { name: 'Account Created', value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`, inline: true },
-        { name: 'Bot?', value: user.bot ? 'Yes' : 'No', inline: true }
-      );
-
-    if (member) {
-      embed.addFields(
-        { name: 'Joined Server', value: `<t:${Math.floor(member.joinedTimestamp / 1000)}:R>`, inline: true },
-        { name: 'Roles', value: member.roles.cache.map(r => r).join(', ') || 'None' }
-      );
-    }
-
-    await interaction.reply({ embeds: [embed] });
+    const user = interaction.options.getUser('user') || interaction.user;
+    await interaction.reply(`WhoIs: ${user.tag}`);
   }
 };
