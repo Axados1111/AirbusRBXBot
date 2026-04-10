95a446c294d88674040ebd85317f78536db6b8ee
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:38:20 +0100

    Update panic.js

--- commands/panic.js
+++ commands/panic.js
@@ -1,30 +1,11 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('panic')
-    .setDescription('Lock all channels in the server (EMERGENCY)')
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    .setDescription('Enable panic mode'),
 
   async execute(interaction) {
-    const channels = interaction.guild.channels.cache;
-
-    await interaction.reply({ content: '🚨 Panic mode activated! Locking all channels...', ephemeral: true });
-
-    channels.forEach(async (channel) => {
-      try {
-        await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
-          SendMessages: false
-        });
-      } catch (err) {
-        console.log(`Couldn't lock ${channel.name}`);
-      }
-    });
-
-    // Optional log
-    const logChannel = interaction.guild.channels.cache.get('LOG_CHANNEL_ID');
-    if (logChannel) {
-      logChannel.send(`🚨 Panic mode activated by ${interaction.user.tag}`);
-    }
+    await interaction.reply('🚨 Panic mode activated (placeholder)');
   }
 };
